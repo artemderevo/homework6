e82a2fa0ffca33110783ac4d4cdc2e1538df73a7
q2q2q8 <art-kash@mail.ru>
2022-10-29 12:18:20 +0300

    второе задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
-        if (clientOS == 1) {
+        int ProductionYear = 2017;
+        if (clientOS == 1 && ProductionYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && ProductionYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0) {
+        }
+        if (clientOS == 0 && ProductionYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && ProductionYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
